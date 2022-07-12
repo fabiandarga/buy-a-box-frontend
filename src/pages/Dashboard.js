7917f2c31f9547ed4481a346632b1412f62f802f
Fabian Darga <kontakt@fabiandarga.de>
2022-07-12 16:42:03 +0200

    Daten laden und filtern

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,12 +1,46 @@
-import React from 'react' 
+import React, {useState, useEffect} from 'react'; 
 import InfoText from '../components/InfoText.js';  
 import FilterBox from '../components/FilterBox.js';
 import PriceChart from '../components/PriceChart.js'; 
 
-function Dashboard() {
+
+
+
+function Dashboard() { 
+
+  const [ allItems , setAllItems]  = useState([]) 
+  useEffect(()=>{
+    fetchRecordsData(); 
+   
+  },[]); //Dependancy Array  
+ 
+  
+  const SCRAPER_PATH = 'https://buy-a-box-backend.herokuapp.com/scraper/all';
+
+  const fetchRecordsData = async()=>{
+    /*'http://localhost:4000/scraper/all'*/ 
+    
+    await fetch(SCRAPER_PATH)
+      .then((response)=>response.json())
+      .then((data)=> { console.log(data); setAllItems(data)}) 
+      .catch((err)=>console.log(err)) 
+  }
+  
+  const selectedSet = ['AFR']
+
+  const filterItems = allItems.filter((scraper, index)=>{  
+
+      return  selectedSet.includes(scraper.code)
+    
+  }); 
+
+  console.log(filterItems);
+
   return (
     <div>  
-      <PriceChart/> 
+      <PriceChart 
+      items={filterItems} 
+      />  
       <FilterBox/>
       <InfoText/>
     </div>
