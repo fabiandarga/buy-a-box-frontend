644bf687309d646d877edbb4d32f36bace51f05a
muhanad-al-musawi <al-musawi-muhanad@hotmail.com>
2022-07-19 16:06:24 +0200

    Footer Hinzufügt

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Header from './components/Header';
+import Footer from './components/Footer';
+import Impressum from './pages/Impressum';
 
 function App() {
   return (
     <div>
-      <Header />
-      <div className="App">
-        <Dashboard />
-      </div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <Header />
+              <div className="App">
+                <Dashboard />
+              </div>
+              <Footer />
+            </div>
+          }
+        />
+        <Route path="/Impressum" element={<Impressum />} />
+      </Routes>
     </div>
   );
 }
