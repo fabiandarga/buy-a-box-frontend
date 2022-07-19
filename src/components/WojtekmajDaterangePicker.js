40a9719387863fc46418646dad251439e178ddd4
muhanad-al-musawi <al-musawi-muhanad@hotmail.com>
2022-07-19 10:47:38 +0200

    Date Range Picker gefixt

--- src/components/WojtekmajDaterangePicker.js
+++ src/components/WojtekmajDaterangePicker.js
@@ -1,22 +1,34 @@
 import React from 'react' 
 import DateRangePicker from "@wojtekmaj/react-daterange-picker";
 
+function formatDate(date) {
+ 
+  let result = '' + date.getFullYear() 
+  const month = date.getMonth()+1 
+  const day = date.getDate() 
+  result += '-'+("0" + month).slice(-2)
+  result += '-'+("0" + day).slice(-2)
+  return result
+} 
+
+console.log(formatDate(new Date())); 
 
 function WojtekmajDaterangePicker(props) { 
   
  // setFrom={props.setFrom} setTo={props.setTo}
  // new Date().toISOString().slice(0,10)
  const onChangeDateRangePickerHandler = (event) => {  
-    props.setFrom(new Date(event[0]).toISOString().slice(0,10)); 
-    props.setTo(new Date(event[1] ).toISOString().slice(0,10)); 
+    props.setFrom(formatDate(new Date(event[0]))); 
+    props.setTo(formatDate(new Date(event[1]))); 
  }
-
+ const valueDate = [new Date(props.from), new Date(props.to)] 
+ console.log('valueDate',valueDate);
   return (
     <div> 
       <h3>Zeitrahmen</h3> 
       <DateRangePicker  
       onChange={onChangeDateRangePickerHandler}  
-      value={[props.to, props.from]} 
+      value={valueDate} 
       />
     </div>
   )
