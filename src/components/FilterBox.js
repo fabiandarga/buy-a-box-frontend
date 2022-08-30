22c33a896018c8cfce1732c3780e2fd968e79ed8
Fabian Darga <kontakt@fabiandarga.de>
2022-08-30 18:41:55 +0200

    Add filter example text

--- src/components/FilterBox.js
+++ src/components/FilterBox.js
@@ -31,8 +31,20 @@ function FilterBox(props) {
           langOptions={selectedLanguage}
           typeOptions={selectedType}
         />
-        <div>
+        <div className="filter-examples">
           <h3>Beispiel #1: Preisvergleich</h3>
+          Wenn du die Preise zu einem bestimmten Produkt vergleichen möchtest, stelle in dem
+          Filter-Popup einfach die gewünschte Erweiterung, Sprache und Produkttyp ein. Am besten
+          wählst u jeweils nur eine Sache aus. Das Feld für &quot;Shops&quot; lässt du einfach leer
+          um die Preise in allen shops zu sehen. Bei der Zeitspanne reicht es aus &quot;letzte
+          Woche&quot; auszuwählen.
+          <h3>Beispiel #2: Preisentwicklung bei Collector Displays</h3>
+          Um zu sehen, wie sich die Preisentwicklung bei Collector Displays entwickeln und wann
+          diese nach einem release ausverkauft sind kannst du folgende Filter benutzen: Wähle z.B.
+          die letzten 4 Erweiterung aus. Bei Produkttyp musst du natürlich &quot;Collector&quot;
+          einstellen. Damit es übersichtlich bleibt wähle nur eine Sprache aus. Das Feld für
+          &quot;Shops&quot; lässt du auch hier leer. Bei der Zeitspanne wähle &quot;letztes
+          Jahr&quot; aus oder sogar länger.
         </div>
       </div>
     </Card>
